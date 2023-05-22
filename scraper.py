2a00b682df9fa2a33e13428ac4315636c8ff0dc6
ilpoa <ilpo.aunio@gmail.com>
2023-05-22 19:54:58 +0300

    pieniä  muokkauksia

--- scraper.py
+++ scraper.py
@@ -1,16 +1,20 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium_stealth import stealth
+from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup as bs
+import requests
 import re as re
 import time
 import pandas as pd
 import csv
 import random
 import json
-
 
 
 
@@ -27,13 +31,14 @@ class User:
 
 
 #ladataan käyttäjät        
-user_list_raw="log.csv"
+user_list_raw="user_list.csv"
 user_list=[]
 with open(user_list_raw) as users:
     for user in users:
         row = user.replace("\n", "")
         data = row.split(";")
-        if data[0]!="username":
+        print(data)
+        if data[0]!="\ufeffusername":
             user_list.append(User(data[0],data[1],data[2]))
     users.close()
 
@@ -53,27 +58,63 @@ PATH = (r"C:\Users\ilpoa\epimetheus\drivers\chromedriver.exe")
 USER=chosen_one.user
 PASSWORD=chosen_one.password
 
+
+#-----PROXY SETTING------
+
+PROXY="185.20.71.38:443"
+
+#------------------------
+
+#-------TARGET-----------
+
+target_company="kesko"
+
+#-----------------------
+
 print(PATH)
+print("Chosen one: ")
 print(USER)
 print(PASSWORD)
+print(f"used {chosen_one.used}")
 
 
-driver = webdriver.Chrome(PATH)
 
-target_company="metsooficial"
 
-driver.get(f"https://www.linkedin.com/company/{target_company}/people/")
+#init for proxy
+options = webdriver.ChromeOptions()
+options.add_argument("start-maximized")
+
+# options.add_argument("--headless")
+
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option('useAutomationExtension', False)
+
+driver = webdriver.Chrome(PATH, options=options)
+
+
+stealth(driver,
+        languages=["en-US", "en"],
+        vendor="Google Inc.",
+        platform="Win32",
+        webgl_vendor="Intel Inc.",
+        renderer="Intel Iris OpenGL Engine",
+        fix_hairline=True,
+        )
+
+#driver.get(f"https://www.linkedin.com/company/{target_company}/people/")
+driver.get("https://bot.incolumitas.com/#botChallenge")
 time.sleep(3)
 
+#email=driver.find_element(By.ID,"username")
+#email.send_keys(USER)
+#password=driver.find_element(By.ID,"password")
+#password.send_keys(PASSWORD)
+#chosen_one.add_use() #lisätään käyttö
+#time.sleep(2+random.randint(0,3)) #jitter
+#password.send_keys(Keys.RETURN)
 
-email=driver.find_element_by_id("username")
-email.send_keys(USER)
-password=driver.find_element_by_id("password")
-password.send_keys(PASSWORD)
-chosen_one.add_use() #lisätään käyttö
-time.sleep(3+random.randint(0,5)) #jitter
-password.send_keys(Keys.RETURN)
-
+time.sleep(30)
+quit()
 
 
 
